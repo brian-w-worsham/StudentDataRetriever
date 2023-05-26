c658335f2b06ed406466e7a7818376652d1e37b3
Brian Worsham <brian.w.worsham@hotmail.com>
2023-05-26 16:08:01 -0500

    Added code to read text file

--- StudentDataRetriever/Program.cs
+++ StudentDataRetriever/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+// read text from StudentNames.txt and display it on the console
+
+// create StreamReader object
+// store the relative path to the file in a string variable   
+string filePath = @"C:\Users\Brian_Worsham\source\StudentDataRetriever\StudentDataRetriever\StudentNames.txt";
+string[] lines = File.ReadAllLines(filePath);
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
+
